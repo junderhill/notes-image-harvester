ddeba70a3013de815b50056df4a99f32710c24c7
Jason Underhill <jason.underhill@bookwisesolutions.com>
2018-01-22 15:03:42 +0000

    Begin setting up project strucuture

---
+++ app.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+var program = require('commander');
+
+program
+    .version("1.0.0")
+    .usage("<file>")
+    .parse(process.argv);
+
+    
---
+++ tests/processor.tests.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const expect = require('chai').expect
+
+describe('Processor module', () =>{
+    describe('processNote', ()=>{
+        describe('when the note contains no images', () => {
+            
+        });
+
+    });
+
+});
